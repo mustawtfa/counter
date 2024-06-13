bbb48f1468830c7190e321d33f446e53e3e8f95d
kodbilmeyenyazilimci <vertexgames23@gmail.com>
2024-06-13 17:43:15 +0300

    M<3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 const port = process.env.PORT || 3000;
 const keep_alive = require('./keep_alive.js');
 
