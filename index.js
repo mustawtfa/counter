dbdc17307297ddcce7da6a2f663093bfa94ed64e
kodbilmeyenyazilimci <vertexgames23@gmail.com>
2024-06-17 16:39:36 +0300

    M<3

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function checkAndFetchLeaderboard() {
     leaderboardFetched = false;
   }
 
-  if (totalSeconds > 604800 && !leaderboardFetched) {
+  if (totalSeconds < 604800 && !leaderboardFetched) {
     fetchLeaderboard();
   }
 
