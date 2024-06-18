a4578035c8b37ba9331594145c4cc8a10feb4395
kodbilmeyenyazilimci <vertexgames23@gmail.com>
2024-06-18 14:48:05 +0300

    M<3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { exec } = require('child_process');
 
 const targetTime = new Date('2024-06-17T00:00:00+03:00');
 const intervalMilliseconds = 7 * 24 * 60 * 60 * 1000;
-let resetCount = 1;
+let resetCount = 2;
 let leaderboardFetched = false;
 let leaderboardResetted = false;
 
