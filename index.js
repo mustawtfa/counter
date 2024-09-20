de60382095fbf05e0788300d3cc500ebe1008896
kodbilmeyenyazilimci <vertexgames23@gmail.com>
2024-09-20 23:42:25 +0300

    M<3

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const { exec } = require('child_process');
 // M<3
-const targetTime = new Date('2024-07-23T08:42:00+03:00');
+const targetTime = new Date('2024-09-21T12.00:00:00+03:00');
 const intervalMilliseconds = 14 * 24 * 60 * 60 * 1000; 
 const extendedIntervalMilliseconds = 60 * 24 * 60 * 60 * 1000; 
 
-let resetCount = 5;
+let resetCount = 6;
 let leaderboardFetched = false;
 let leaderboardResetted = false;
 
-const leaderboardUrl = 'https://lcv2-server.danqzq.games/get?publicKey=5830625ddabd61108a0b079b05ab593914b104bd796a642200be28187e16022e';
+const leaderboardUrl = 'https://lcv2-server.danqzq.games/get?publicKey=2783dcf10b8e257284355d32d80275380bc83e3f98d6527dbc3433c01f237700';
 
 app.get('/', (req, res) => {
   res.send('Sayaç uygulamasına hoş geldiniz!');
@@ -25,7 +25,7 @@ app.get('/counter', (req, res) => {
   let elapsedMilliseconds = now - targetTime;
   let totalSeconds;
   
-  if (resetCount === 5) {
+  if (resetCount === 6) {
     totalSeconds = (elapsedMilliseconds % extendedIntervalMilliseconds) / 1000;
   } else {
     totalSeconds = (elapsedMilliseconds % intervalMilliseconds) / 1000;
@@ -61,14 +61,14 @@ function checkAndFetchLeaderboard() {
   let elapsedMilliseconds = now - targetTime;
   let totalSeconds;
   
-  if (resetCount === 5) {
+  if (resetCount === 6) {
     totalSeconds = (elapsedMilliseconds % extendedIntervalMilliseconds) / 1000;
   } else {
     totalSeconds = (elapsedMilliseconds % intervalMilliseconds) / 1000;
   }
 
-  if ((resetCount === 5 && totalSeconds >= (60 * 24 * 60 * 60 - 60)) || 
-      (resetCount > 5 && totalSeconds >= (14 * 24 * 60 * 60 - 60)) && 
+  if ((resetCount === 6 && totalSeconds >= (60 * 24 * 60 * 60 - 60)) || 
+      (resetCount > 6 && totalSeconds >= (14 * 24 * 60 * 60 - 60)) && 
       !leaderboardFetched) {
     fetchLeaderboard();
   }
